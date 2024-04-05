48798cdc53e09e73a35f53c6e07ee22ab91b0f91
Leonid Frolov <froleod@gmail.com>
2024-04-05 16:19:30 +0300

    working app with security, css embedded in html code, without images in yandex map, without connection of the trip with the user

--- src/main/java/com/ldf/carsharingcompany/Config/SecurityConfig.java
+++ src/main/java/com/ldf/carsharingcompany/Config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,7 +35,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/new-user", "/login").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/new-user", "/login", "/static/**").permitAll()
+//                        .requestMatchers("/static/**").permitAll()
                         .requestMatchers("/").authenticated()
                         .requestMatchers("/booking").authenticated()
                         .requestMatchers("/processBooking").authenticated()
@@ -43,6 +45,11 @@ public class SecurityConfig {
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .build();
     }
+
+//    @Bean
+//    WebSecurityCustomizer configureWebSecurity() {
+//        return (web) -> web.ignoring().requestMatchers("src/main/resources/static/**");
+//    }
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
